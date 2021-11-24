206c7529bbe22002cd50996ec55c184ddd723800
Israel <israeladefidipe@gmail.com>
2021-11-24 04:11:46 +0100

    Store � data on our smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,27 @@ import "hardhat/console.sol"; // Some magic given to us by Hardhat to do some co
 
 // Once we initialize this contract for the first time, that constructor will run and print out that line
 contract WavePortal {
+    uint256 totalWaves; // this variable is special because it's called a "state variable" and it's cool because it's stored permanently in contract storage.
+
     constructor(){
+        
         console.log("I am a contract and I am smart");
     } 
+
+    function wave() public{
+        totalWaves += 1;
+        console.log("%S has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (unit256){
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
 }
+
+
+// The blockchain = Think of it as a cloud provider, kinda like AWS, but it's owned by no one. It's run by compute power from mining machines all over the world. Usually these people are called miners and we pay them to run our code!
+
+// A smart contract = Kinda like our server's code with different functions people can hit.
+
+// the public keyword: Think of this like a public API endpoint :).
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,27 @@
 
-const main = async () => {
+
+
+  const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners(); // grabbing the wallet address of contract owner and  a random wallet address and called it randomPerson
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); // This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+  
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+  
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves(); //when contract is call this get intial no of total waves
+    
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait(); // calls d wave
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+  
+    waveCount = await waveContract.getTotalWaves(); // will show the no of waves after the wave
   };
   
   const runMain = async () => {
