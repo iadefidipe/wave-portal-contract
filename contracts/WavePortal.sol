f7174d5a5014ce580c648d23fa1b3f3ff38192e7
Israel <israeladefidipe@gmail.com>
2021-11-27 02:40:22 +0100

    funded contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,32 +1,120 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0; // this is the version of solidity compiler we want to use
+pragma solidity ^0.8.0;
 
-import "hardhat/console.sol"; // Some magic given to us by Hardhat to do some console logs in our contract.
+import "hardhat/console.sol";
 
-// Once we initialize this contract for the first time, that constructor will run and print out that line
 contract WavePortal {
-    uint256 totalWaves; // this variable is special because it's called a "state variable" and it's cool because it's stored permanently in contract storage.
+    uint256 totalWaves;
 
-    constructor(){
-        
-        console.log("I am a contract and I am smart");
-    } 
+    
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
-    function wave() public{
+    
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+  
+    Wave[] waves;
+
+    constructor() payable {
+        console.log("I AM SMART CONTRACT. POG.");
+    }
+
+    
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%S has waved!", msg.sender);
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
 
+// pragma solidity ^0.8.0; // this is the version of solidity compiler we want to use
 
-// The blockchain = Think of it as a cloud provider, kinda like AWS, but it's owned by no one. It's run by compute power from mining machines all over the world. Usually these people are called miners and we pay them to run our code!
+// import "hardhat/console.sol"; // Some magic given to us by Hardhat to do some console logs in our contract.
 
-// A smart contract = Kinda like our server's code with different functions people can hit.
+// // Once we initialize this contract for the first time, that constructor will run and print out that line
+// contract WavePortal {
+//     uint256 totalWaves; // this variable is special because it's called a "state variable" and it's cool because it's stored permanently in contract storage.
 
-// the public keyword: Think of this like a public API endpoint :).
+//     event NewWave(address indexed from, uint256 timestamp, string message);
+
+//     /*
+//      * I created a struct here named Wave.
+//      * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+//      */
+//     struct Wave {
+//         address waver; // The address of the user who waved.
+//         string message; // The message the user sent.
+//         uint256 timestamp; // The timestamp when the user waved.
+//     }
+
+//     /*
+//      * I declare a variable waves that lets me store an array of structs.
+//      * This is what lets me hold all the waves anyone ever sends to me!
+//      */
+//     Wave[] waves;
+
+//     constructor() {
+//         console.log("I am a contract and I am smart");
+//     }
+
+//     function wave() public {
+//         totalWaves += 1;
+//         console.log("%S has waved!", msg.sender);
+
+//         waves.push(Wave(msg.sender, _message, block.timestamp)); // pushing required data to the wave array
+
+//         emit NewWave(msg.sender, block.timestamp, _message);
+//     }
+
+//     /*
+//      * the function getAllWaves which will return the struct array, waves, to us.
+//      * This will make it easy to retrieve the waves from our website!
+//      */
+//     function getAllWaves() public view returns (Wave[] memory) {
+//         return waves;
+//     }
+
+//     function getTotalWaves() public view returns (uint256) {
+//         console.log("We have %d total waves!", totalWaves);
+//         return totalWaves;
+//     }
+// }
+
+// // The blockchain = Think of it as a cloud provider, kinda like AWS, but it's owned by no one. It's run by compute power from mining machines all over the world. Usually these people are called miners and we pay them to run our code!
+
+// // A smart contract = Kinda like our server's code with different functions people can hit.
+
+// // the public keyword: Think of this like a public API endpoint :).
+
+// require which basically checks to see that some condition is true. If it's not true, it will quit the function and cancel the transaction. It's like a fancy if statement!
--- scripts/run.js
+++ scripts/run.js
@@ -1,40 +1,89 @@
 
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
+  /*
+   * Get Contract balance
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-  const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners(); // grabbing the wallet address of contract owner and  a random wallet address and called it randomPerson
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); // This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-  
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-  
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves(); //when contract is call this get intial no of total waves
-    
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait(); // calls d wave
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
+  /*
+   * Send Wave
+   */
+  let waveTxn = await waveContract.wave('A message!');
   await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+// ? first version of run.js
+  // const main = async () => {
+  //   const [owner, randomPerson] = await hre.ethers.getSigners(); // grabbing the wallet address of contract owner and  a random wallet address and called it randomPerson
+  //   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); // This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory
+  //   const waveContract = await waveContractFactory.deploy();
+  //   await waveContract.deployed();
   
-    waveCount = await waveContract.getTotalWaves(); // will show the no of waves after the wave
-  };
+  //   console.log("Contract deployed to:", waveContract.address);
+  //   console.log("Contract deployed by:", owner.address);
   
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
+  //   let waveCount;
+  //   waveCount = await waveContract.getTotalWaves(); //when contract is call this get intial no of total waves
+    
+  //   let waveTxn = await waveContract.wave();
+  //   await waveTxn.wait(); // calls d wave
+
+  //   waveTxn = await waveContract.connect(randomPerson).wave();
+  //   await waveTxn.wait();
+
+  //   waveCount = await waveContract.getTotalWaves();
   
-  runMain();
+  //   waveCount = await waveContract.getTotalWaves(); // will show the no of waves after the wave
+  // };
+  
+  // const runMain = async () => {
+  //   try {
+  //     await main();
+  //     process.exit(0);
+  //   } catch (error) {
+  //     console.log(error);
+  //     process.exit(1);
+  //   }
+  // };
+  
+  // runMain();
 
 //? The Hardhat Runtime Environment, or HRE for short, is an object containing all the functionality that Hardhat exposes when running a task, test or script. In reality, Hardhat is the HRE.
 // * So what does this mean? Well, every time you run a terminal command that starts with npx hardhat you are getting this hre object built on the fly using the hardhat.config.js specified in your code!
